8a4633a6e304ad0756ee3695bf711c3c8f9bc016
Mattia de' Michieli Vitturi <demichie@gmail.com>
2024-02-20 15:02:30 +0100

    Update submitMPIcase.sh

--- run/synthTopo/submitMPIcase.sh
+++ run/synthTopo/submitMPIcase.sh
@@ -28,12 +28,15 @@ changeDictionary
 # create zones based on stl files of closed surfaces (crater, conduit,...)
 topoSet -dict topoSetDict-conduit
 
-# 
+# set fvSolution and cloudProperties for initialization of atm. profile and ballistics
 cp ./system/fvSolution.init ./system/fvSolution
 cp ./constant/cloudProperties.init ./constant/cloudProperties
 
+# remove previous 0 folder and copy not-initialized fields
 rm -rf 0
 cp -r org.0 0
+
+# (first run for initialization of the solution: fileds and ballistics) 
 
 #FOR PARALLEL RUN:
 #sbatch MPIJob_init.script
@@ -41,8 +44,11 @@ cp -r org.0 0
 
 #FOR SCALAR RUN:
 foamRun
+
+# set different values in the crater/conduit zone
 setFields
 
+# set the run parameters for the actual simulation
 cp ./system/controlDict.run system/controlDict
 cp ./system/fvSolution.run system/fvSolution
 cp ./constant/cloudProperties.run ./constant/cloudProperties
