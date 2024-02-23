e6e754bc7306cfb8844c197f33b0b62b92153eea
Mattia de' Michieli Vitturi <demichie@gmail.com>
2024-02-23 16:33:25 +0100

    Update plotFieldsMap.py

--- run/synthTopo/plotFieldsMap.py
+++ run/synthTopo/plotFieldsMap.py
@@ -1,33 +1,52 @@
 from matplotlib.colors import BoundaryNorm
 from matplotlib.colors import LightSource
-try:
-    from preprocessing.ASCtoSTLdict import domain_size_x
-    from preprocessing.ASCtoSTLdict import domain_size_y
-except:
-    print('') 
-    
-from preprocessing.ASCtoSTLdict import xc
-from preprocessing.ASCtoSTLdict import yc
-from preprocessing.ASCtoSTLdict import DEM_file
 
+from preprocessing.ASCtoSTLdict import DEM_file
 
 import vtk
 import os
 import sys
-from vtk.numpy_interface import dataset_adapter as dsa
-from vtk.util import numpy_support
 import numpy as np
-from numpy import linalg as LA
 import matplotlib.pyplot as plt
-from scipy.stats import gaussian_kde
 from linecache import getline
 import pandas as pd
 from scipy.interpolate import griddata
+import netCDF4
 
 sys.path.insert(0, './preprocessing')
 
-toll = 1.0
-step_dens = 50.0
+cell_size_map = 10.0
+
+
+# Print iterations progress
+def printProgressBar(iteration,
+                     total,
+                     prefix='',
+                     suffix='',
+                     decimals=1,
+                     bar_length=20):
+    """
+    Call in a loop to create terminal progress bar
+
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : number of decimals in percent complete (Int)
+        bar_length  - Optional  : character length of bar (Int)
+    """
+    str_format = "{0:." + str(decimals) + "f}"
+    percents = str_format.format(100 * (iteration / float(total)))
+    filled_length = int(round(bar_length * iteration / float(total)))
+    bar = '█' * filled_length + '-' * (bar_length - filled_length)
+
+    sys.stdout.write('\r%s |%s| %s%s %s' %
+                     (prefix, bar, percents, '%', suffix)),
+
+    if iteration == total:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
 
 
 def fmt(x, pos):
@@ -46,26 +65,23 @@ def readerVTK(filename):
     reader.ReadAllTensorsOn()
     reader.ReadAllFieldsOn()
     reader.Update()
-    
+
     data = reader.GetOutput()
-    
-    
+
     npoints = data.GetNumberOfPoints()
-    # print('npoints',npoints)
-    point = data.GetPoint(0)
+
     d = data.GetPointData()
-    
-    position_array = data.GetPoint
-    alpha_array = d.GetArray("alpha.particles")  
-    
-    position = np.zeros((npoints,3))
+
+    alpha_array = d.GetArray("alpha.particles")
+
+    position = np.zeros((npoints, 3))
     alpha_particles = np.zeros(npoints)
- 
+
     for n in range(npoints):
 
-        position[n,:] = data.GetPoint(n)
+        position[n, :] = data.GetPoint(n)
         alpha_particles[n] = np.array(alpha_array.GetTuple(n))
-         
+
     return position, alpha_particles
 
 
@@ -176,10 +192,9 @@ def main():
     VTKexists = os.path.exists(check_dir)
 
     foamCommand = "foamToVTK -fields '()' -noInternal " + \
-         "-noFaceZones -excludePatches '(atm top terrain_in terrain_out)'"
-         
-    print(foamCommand) 
-        
+        "-noFaceZones -excludePatches '(atm top terrain_in terrain_out)'"
+
+    print(foamCommand)
 
     if VTKexists:
 
@@ -200,60 +215,101 @@ def main():
 
     working_dir = current_dir + '/VTK/terrain_out'
     files = os.listdir(working_dir)
-    n_files = len(files)
+
     file_idx = []
     for filename in files:
-        
+
         file_idx.append(float(filename.split('_')[-1].rsplit('.', 1)[0]))
 
     sorted_files = [files[i] for i in np.argsort(file_idx)]
-    sorted_times = [dt * i for i in range(len(file_idx))]
-
-    # print('Times read',sorted_times)
 
     full_filename = working_dir + '/' + sorted_files[-1]
 
     position, alpha_max = readerVTK(full_filename)
 
-    n_times = n_files
+    points = position[:, 0:2]
+
+    x_map_min = np.amin(position[:, 0])
+    x_map_max = np.amax(position[:, 0])
+    y_map_min = np.amin(position[:, 1])
+    y_map_max = np.amax(position[:, 1])
+
+    x_map = np.arange(x_map_min, x_map_max, step=cell_size_map)
+    y_map = np.arange(y_map_min, y_map_max, step=cell_size_map)
+
+    extent_map = x_map_min - 0.5 * cell_size_map, x_map_max + 0.5 * \
+        cell_size_map, y_map_min - 0.5 * cell_size_map, \
+        y_map_max + 0.5 * cell_size_map
+
+    print('extent_map', extent_map)
+
+    xx, yy = np.meshgrid(x_map, y_map)
+
+    ncfilename = current_dir_name + '_solidMap.nc'
+
+    ncfile = netCDF4.Dataset(ncfilename, mode='w', format='NETCDF4_CLASSIC')
+
+    ncfile.createDimension('x', xx.shape[1])
+    ncfile.createDimension('y', xx.shape[0])
+    ncfile.createDimension('time', None)
+
+    ncfile.title = current_dir_name + ' output'
+
+    ncfile.Conventions = "CF-1.0"
+    ncfile.subtitle = "My model data subtitle"
+    ncfile.anything = "write anything"
+
+    x = ncfile.createVariable('x', np.float64, ('x', ))
+    x.long_name = 'x dim'
+    x.units = 'meters'
+
+    y = ncfile.createVariable('y', np.float64, ('y', ))
+    y.long_name = 'y dim'
+    y.units = 'meters'
+
+    time = ncfile.createVariable('time', np.float64, ('time', ))
+    time.long_name = 'Time'
+    time.units = 'seconds'
+
+    # note: unlimited dimension is leftmost
+    alphaP = ncfile.createVariable('alphaP', np.float64, ('time', 'y', 'x'))
+    alphaP.standard_name = 'alfa particles'
+    alphaP.units = 'volume fraction'
+
+    x[:] = xx[0, :]
+    y[:] = yy[:, 0]
 
     for i, filename in enumerate(sorted_files[:]):
 
+        printProgressBar(i, len(sorted_files) - 1)
+
+        time[i] = dt * i
         full_filename = working_dir + '/' + filename
         # print(filename)
         position, alpha_particles = readerVTK(full_filename)
-        alpha_max = np.maximum(alpha_max,alpha_particles)
+        zz = griddata(points, alpha_particles, (xx, yy), method='nearest')
 
+        alphaP[i, :, :] = zz
 
-    print(np.amin(alpha_particles))
-    print(np.amax(alpha_particles))
+        alpha_max = np.maximum(alpha_max, alpha_particles)
 
-    step_dens = 20.0
+    print(ncfile)
 
-    x_dens_min = np.amin(position[:,0])
-    x_dens_max = np.amax(position[:,0])
-    y_dens_min = np.amin(position[:,1])
-    y_dens_max = np.amax(position[:,1])
+    ncfile.close()
+    print('Dataset is closed!')
 
-    x_density = np.arange(x_dens_min, x_dens_max, step=step_dens)
-    y_density = np.arange(y_dens_min, y_dens_max, step=step_dens)
+    print(np.amin(alpha_max))
+    print(np.amax(alpha_max))
 
-    extent_density = x_dens_min, x_dens_max, y_dens_min, y_dens_max
+    values = alpha_max[:]
 
-    print('extent_density', extent_density)
+    zz = griddata(points, values, (xx, yy), method='nearest')
 
-    xx, yy = np.meshgrid(x_density, y_density)
-
-    
-    points = position[:,0:2]
-    values = alpha_particles[:]
-    zz = griddata(points, values, (xx, yy), method='linear')
-
-    zz[zz<1.e-6] = np.nan
+    zz[zz < 1.e-6] = np.nan
 
     # print(np.amin(zz))
     # print(np.amax(zz))
-    
+
     xmin = np.amin(Xinit) - 0.5 * cell
     xmax = np.amax(Xinit) + 0.5 * cell
 
@@ -262,16 +318,11 @@ def main():
 
     fig, ax = plt.subplots()
 
-    im = ax.imshow(ls.hillshade(np.flipud(Zinit),
-                                vert_exag=1.0,
-                                dx=cell,
-                                dy=cell),
-                   cmap='gray',
-                   extent=extent)
+    ax.imshow(ls.hillshade(np.flipud(Zinit), vert_exag=1.0, dx=cell, dy=cell),
+              cmap='gray',
+              extent=extent)
 
     ax.set_aspect('equal', 'box')
-    
-    im_ratio = (ymax - ymin) / (xmax - xmin)
 
     levels = np.linspace(-6.0, 1.0, 11)
     label_str = 'Probabilty [0;1]'
@@ -279,13 +330,12 @@ def main():
     cmap = plt.get_cmap('viridis')
     norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
 
-    # ax.scatter(x+xc,y+yc,c=z,s=3,alpha=0.1,edgecolors='none')
-
     p1 = ax.imshow(np.flipud(np.log10(zz)),
                    cmap=cmap,
+                   norm=norm,
                    interpolation='nearest',
-                   extent=extent_density,
-                   alpha=0.65)
+                   extent=extent_map,
+                   alpha=0.5)
 
     ax.set_xlim(xmin, xmax)
     ax.set_ylim(ymin, ymax)
@@ -298,22 +348,21 @@ def main():
     title = 'Solid particles'
     plt.title(title)
 
-    png_file = current_dir_name + '_solid.png'
+    png_file = current_dir_name + '_solidMap.png'
 
     plt.savefig(png_file, dpi=200)
-    plt.close(fig)
 
     nd = -9999
 
-    zz[zz == np.nan] = nd
+    zz[np.isnan(zz)] = nd
 
-    asc_file = current_dir_name + '_solid.asc'
+    asc_file = current_dir_name + '_solidMap.asc'
 
     header = "ncols     %s\n" % xx.shape[1]
     header += "nrows    %s\n" % xx.shape[0]
-    header += "xllcorner " + str(x_dens_min) + "\n"
-    header += "yllcorner " + str(y_dens_min) + "\n"
-    header += "cellsize " + str(step_dens) + "\n"
+    header += "xllcorner " + str(x_map_min - 0.5 * cell_size_map) + "\n"
+    header += "yllcorner " + str(y_map_min - 0.5 * cell_size_map) + "\n"
+    header += "cellsize " + str(cell_size_map) + "\n"
     header += "NODATA_value " + str(nd)
 
     np.savetxt(asc_file,
@@ -322,7 +371,8 @@ def main():
                fmt='%1.5f',
                comments='')
 
-    # plt.show()
+    plt.show()
+    plt.close(fig)
 
 
 if __name__ == '__main__':
